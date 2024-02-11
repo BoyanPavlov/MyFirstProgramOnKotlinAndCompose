e86694e8e627f0661ebd65be989287e42f4fded0
Boyan  Pavlov <boyan.v.pavlov@gmail.com>
2024-02-11 21:42:05 +0200

    Improving styles and importing fonts  from Google

--- app/src/main/java/com/example/myfirstprogram/MainActivity.kt
+++ app/src/main/java/com/example/myfirstprogram/MainActivity.kt
@@ -1,22 +1,17 @@
 package com.example.myfirstprogram
 
-import android.graphics.drawable.shapes.OvalShape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -26,17 +21,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -95,7 +93,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             modifier = modifier
                 .padding(10.dp)
                 .border(7.dp, color = Color.Yellow)
-                .offset(-30.dp,-30.dp)
+                .offset(-30.dp, -30.dp)
                 .padding(24.dp)
                 .background(Color.LightGray)
                 .border(2.dp, Color.Black, shape = CircleShape)
@@ -151,6 +149,58 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 fontStyle = FontStyle.Italic,
                 fontFamily = FontFamily.Serif,
             )
+        }
+        Box(
+            modifier = modifier
+                .align(Alignment.TopCenter)
+                .offset(0.dp, 70.dp)
+                .padding(16.dp)
+                .background(color = Color.Green)
+                .fillMaxWidth(0.5f)
+                .fillMaxHeight(0.3f)
+                .border(
+                    width = 10.dp,
+                    color = Color.DarkGray,
+                    shape = RectangleShape
+                )
+                .padding(17.dp)
+        ) {
+            //When you import text - it must  be lower case and it  should have "_"
+            val importedFontFromGoogle = FontFamily(Font(R.font.sixtyfour_regular))
+
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Red,
+                            fontSize = 18.sp,
+                        )
+                    )
+                    {
+                        append("J")
+                    }
+                    append("etpack ")
+                    /////////////////////
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Black,
+                            fontSize = 18.sp,
+                        )
+                    )
+                    {
+                        append("C")
+                    }
+                    append("ompose")
+                },
+                color = Color.White,
+                fontSize = 14.sp,
+                fontFamily = importedFontFromGoogle,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic,
+                textAlign = TextAlign.Center,
+                //textDecoration = TextDecoration.LineThrough,
+                textDecoration = TextDecoration.Underline,
+                )
         }
     }
 }
